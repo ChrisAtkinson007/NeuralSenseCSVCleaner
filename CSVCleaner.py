99f54769267d5b4a5ebfb45fac0849f6d5a991f3
Chris Atkinson <66328714+ChrisAtkinson007@users.noreply.github.com>
2021-02-18 15:27:52 +0200

    Add files via upload

--- CSVCleaner.py
+++ CSVCleaner.py
@@ -7,9 +7,20 @@ try:
 except:
     print("file already exists")
 
+ClientCode=input("Please input a 4 character client code: ")
+while len(ClientCode)!=4:
+    ClientCode=input("Incorrect Client Code Length, Please try again")
+
+ProjDate=input("Please input 6 digits yy/mm/dd representing the project date: ")
+while len(ProjDate)!=6:
+    ProjDate=input("Incorrect Date Length, Please try again")
+
 for filename in os.listdir(os.getcwd()):
     if filename.endswith('.csv'):
         print(filename)
+        RespCode = filename[-6:-4]
+        Fname = ClientCode + "_" + ProjDate + "_" + RespCode + "_e.csv"
+        print(Fname)
         #Read in files from working directory
         data=pd.read_csv(filename)
         #Isolate first row of data, as the new headers
@@ -28,6 +39,6 @@ for filename in os.listdir(os.getcwd()):
         data=data.drop(['Timestamp', 'Row', 'ESource', 'SlideEvent', 'StimType', 'Duration', 'CollectionPhase', 'EventSource'], axis=1)
         #Removing all NaN rows
         data=data.dropna()
-        direc=path + '\proc' + filename
+        direc=path + "\\"+Fname
         print(direc)
         data.to_csv(direc)
